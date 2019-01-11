1a4706725661fc1dbe2af048945b73f9cfcaf784
Cvanzy <cao.zhi@husky.neu.edu>
2019-01-11 11:38:15 -0500

    CHange /api/hello/string to hello cs4500!

--- src/main/java/edu/neu/cs4500/controllers/hello/HelloController.java
+++ src/main/java/edu/neu/cs4500/controllers/hello/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/api/hello/string")
     public String sayHello() {
-        return "Hello Zhiyuan Cao!";
+        return "Hello CS4500!";
     }
     @RequestMapping("/api/hello/object")
     public HelloObject sayHelloObject() {
